4f6dcd1bc44544d3a9e3473dead4ed76ea1d2c2e
faukiofficial <faukiofficial@gmail.com>
2024-06-03 15:31:49 +0700

    add stowatch

--- main.js
+++ main.js
@@ -1,3 +1,49 @@
+// Stopwatch
+let [milliseconds, seconds, minutes, hours] = [0,0,0,0];
+let timeRef = document.querySelector(".timerdisplay");
+let int = null;
+
+document.querySelector("#start").onclick = function(){
+    if(int !== null){
+        clearInterval(int);
+    }
+    int = setInterval(displayTimer, 10);
+};
+
+document.querySelector("#pause").onclick = function(){
+    clearInterval(int);
+};
+
+document.querySelector("#reset").onclick = function(){
+    clearInterval(int);
+    [milliseconds, seconds, minutes, hours] = [0,0,0,0];
+    timeRef.innerHTML = "00 : 00 : 00 : 000";
+};
+
+function displayTimer() {
+    milliseconds += 10;
+    if(milliseconds == 1000){
+        milliseconds = 0;
+        seconds++;
+        if(seconds == 60){
+            seconds = 0;
+            minutes++;
+            if(minutes == 60){
+                minutes = 0;
+                hours++;
+            }
+        }
+    }
+
+    let h = hours < 10 ? "0" + hours : hours;
+    let m = minutes < 10 ? "0" + minutes : minutes;
+    let s = seconds < 10 ? "0" + seconds : seconds;
+    let ms = milliseconds < 10 ? "00" + milliseconds : milliseconds < 100 ? "0" + milliseconds : milliseconds;
+
+    timeRef.innerHTML = `${h} : ${m} : ${s} : ${ms}`
+}
+
+// To Do List
 document.querySelector("#push").onclick = function(){
     if(document.querySelector("#newtask input").value.length == 0){
         alert("Please input a task")
